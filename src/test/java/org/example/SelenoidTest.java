0c08b32a7e249dfd4f8e71810d5637fc90b2e313
WaqasSQA <jjonsnoww1291@gmail.com>
2024-07-01 13:27:48 +0500

    first

---
+++ src/test/java/org/example/SelenoidTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SelenoidTest {
+    public WebDriver driver;
+
+    @Test (groups = "web")
+    public void test1(){
+        try {
+            driver.get("http://www.google.com");
+            Thread.sleep(10000);
+            System.out.println("Title: " + driver.getTitle());
+            driver.quit();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @BeforeClass
+            public void setup() throws MalformedURLException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        options.setCapability("selenoid:options", selenoidOptions);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+    }
+}
+
+
